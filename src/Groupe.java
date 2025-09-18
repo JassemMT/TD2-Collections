1324576800646cde5f0a8539776229546d77044d
Jassem <jassem.tamourgh9@etu.univ-lorraine.fr>
2025-09-18 11:45:03 +0200

    Groupe

--- src/Groupe.java
+++ src/Groupe.java
@@ -1,6 +1,56 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Groupe {
-    List<Etudiant> etudiants;
-    Formation formation;
+    private List<Etudiant> etudiants;
+    private Formation formation;
+
+    public Groupe(Formation formation) {
+        this.etudiants = new ArrayList<>();
+        this.formation = formation;
+    }
+
+    public void ajouterEtudiant(Etudiant etudiant) {
+        if (etudiant.getFormation().equals(formation)) {
+            etudiants.add(etudiant);
+        } else {
+            System.out.println("étudiant non présent dans la formation");
+        }
+    }
+
+    public void supprimerEtudiant(Etudiant etudiant) {
+        etudiants.remove(etudiant);
+    }
+
+    public Formation getFormation() {
+        return this.formation;
+    }
+
+    public List<Etudiant> getEtudiants() {
+        return this.etudiants;
+    }
+
+    public double moyenneMatiere(Matiere m) {
+        double total = 0;
+        int count = 0;
+
+        for (Etudiant etu : etudiants) {
+            total += etu.moyenneMatiere(m);
+            count++;
+        }
+
+        return (count == 0) ? 0 : total / count;
+    }
+
+    public double moyenneGenerale() {
+        double total = 0;
+        int count = 0;
+
+        for (Etudiant etu : etudiants) {
+            total += etu.moyenneGenerale();
+            count++;
+        }
+
+        return (count == 0) ? 0 : total / count;
+    }
 }
