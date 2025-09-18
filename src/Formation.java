aca4ed9e7bbf04f15d68755bf00792f8d6160869
Thomas Goffin <thomas.goffin.001@student.uni.lu>
2025-09-18 11:14:57 +0200

    vi

--- src/Formation.java
+++ src/Formation.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Formation {
     private String idFormation;
@@ -7,7 +8,36 @@ public class Formation {
     public Formation(String idFormation) {
         this.matieres = new HashMap<>();
         this.idFormation = idFormation;
-
     }
 
+    public String getIdFormation() {
+        return this.idFormation;
+    }
+
+    public void setIdFormation(String idFormation) {
+        this.idFormation = idFormation;
+    }
+
+    public void ajouterMatiere(Matiere matiere,Double note) {
+        this.matieres.put(matiere,note);
+    }
+
+    public void afficherMatieres() {
+        System.out.println(this.matieres);
+    }
+    @Override
+    public String toString() {
+        return "Formation{" + "idFormation='" + idFormation + '\'' + ", matieres=" + matieres + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Formation formation)) return false;
+        return Objects.equals(idFormation, formation.idFormation) && Objects.equals(matieres, formation.matieres);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idFormation, matieres);
+    }
 }
--- src/Identite.java
+++ src/Identite.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Identite {
     private String nip;
     private String nom;
@@ -7,5 +9,49 @@ public class Identite {
         this.nip = nip;
         this.nom = nom;
         this.prenom = prenom;
+    }
+
+    public String getNip() {
+        return this.nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Identite identite)) return false;
+        return Objects.equals(nip, identite.nip) && Objects.equals(nom, identite.nom) && Objects.equals(prenom, identite.prenom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nip, nom, prenom);
+    }
+
+    @Override
+    public String toString() {
+        return "Identite{" +
+                "nip='" + nip + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                '}';
     }
 }
